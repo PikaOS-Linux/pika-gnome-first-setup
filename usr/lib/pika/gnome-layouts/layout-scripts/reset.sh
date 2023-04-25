de44ea9f3965240087fb8726c2ebf5679ef30318
ward <ward@pop-os.localdomain>
2023-04-25 15:37:55 +0300

    first commit

--- usr/lib/pika/gnome-layouts/layout-scripts/reset.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-for ext in $(gnome-extensions list); do
-    gnome-extensions reset $ext;
-    gnome-extensions disable $ext;
-done
-gnome-extensions enable pika-darkmode@pika.com
-gnome-extensions enable ubuntu-appindicators@ubuntu.com
--- usr/lib/pika/gnome-layouts/reload.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-	if [[ "$RELOG_NEEDED" == 1 ]]; then
-		if zenity --question --text="New extensions have been installed!, reload required!"
-		then
-		gnome-session-quit --force --no-prompt
-		fi
-	fi
